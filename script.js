454dc09bc26c35f2f043a2aa54c2aa0509935dda
hosein <hoseinfrpbypass@gmail.com>
2022-12-11 19:10:26 +0330

    script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let a = document.getElementsByTagName("a");
 const tag_a = document.querySelector(".footer")
 
-const container = document.addEventListener("click",()=>{
-    tag_a.style.backgroundcolor= "black"
+const container = document.addEventListener("click", () => {
+    tag_a.style.backgroundColor = "black"
+    tag_a.style.color = "green"
 })
