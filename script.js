df97019c7245ff1d54531cb3fb30bbb83e1c900f
hosein <hoseinfrpbypass@gmail.com>
2022-12-11 18:13:24 +0330

    script

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let a = document.getElementsByTagName("a");
 const tag_a = document.querySelector(".footer")
 
 const container = document.addEventListener("click",()=>{
-    tag_a.style.color= "black"
+    tag_a.style.backgroundcolor= "black"
 })
